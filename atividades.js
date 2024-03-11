ca73a58cc3f3f54a5889fef9b8bb5c6ca14ddb19
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-11 14:01:22 -0300

    Parte 1

---
+++ atividades.js
@@ -0,0 +1,12 @@
+let saldo = 0;
+let deposito = 1000;
+let saque = 100;
+
+let operacao = saldo;
+console.log(operacao);
+
+ operacao = saldo += deposito;
+console.log(operacao);
+
+ operacao = saldo -= saque;
+console.log(operacao);
