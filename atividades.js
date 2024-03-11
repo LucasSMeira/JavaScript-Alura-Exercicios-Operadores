2f7b6c2ceeb4578246df5664aec01299a84a03ea
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-11 14:15:32 -0300

    Parte 2

--- atividades.js
+++ atividades.js
@@ -10,3 +10,7 @@ console.log(operacao);
 
  operacao = saldo -= saque;
 console.log(operacao);
+
+const num = 10
+const ehpar = num %2 === 0? "O número é par" : "O número não é par";
+console.log(ehpar);
